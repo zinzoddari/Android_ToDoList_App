c85a1d493a909706573ad37d5d3c5611261c0eed
zinzo <zinzoddari@gmail.com>
2018-11-25 01:20:00 +0900

    버튼 추가 완료

---
+++ app/src/main/java/com/example/todolist/todolist/Data.java
@@ -0,0 +1,9 @@
+package com.example.todolist.todolist;
+
+import java.util.Date;
+
+public class Data
+{
+    public String strTitle;
+    public String strDate;
+}
--- app/src/main/java/com/example/todolist/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/todolist/MainActivity.java
@@ -5,26 +5,25 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
-    public static final int sub = 1001; /*다른 액티비티를 띄우기 위한 요청코드(상수)*/
+    private ListView m_oListView = null;
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener()
-    {
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item)
         {
             switch (item.getItemId())
             {
-                case R.id.navigation_home:
+                case R.id.navigation_hide:
                     Intent intent = new Intent(getApplicationContext(),MainHistory.class);
                     startActivity(intent);
                     return true;
@@ -41,5 +40,20 @@ public class MainActivity extends AppCompatActivity
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        ArrayList<Data> oData = new ArrayList<>();
+        for (int i=0; i<1000; ++i)
+        {
+            Data oItem = new Data();
+            oItem.strTitle = "데이터".toString();
+            oItem.strDate = "dd".toString();
+            oData.add(oItem);
+        }
+
+        // ListView, Adapter 생성 및 연결 ------------------------
+        m_oListView = (ListView)findViewById(R.id.listView);
+        ListAdapter oAdapter = new ListAdapter(oData);
+        m_oListView.setAdapter(oAdapter);
     }
+
 }
